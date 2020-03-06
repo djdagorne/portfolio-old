e449ce71e6d531a4e8550755e2ff25e392b53295
Dexter Dagorne <dj.dagorne@gmail.com>
2020-03-06 13:14:20 -0500

    updated projects

--- index.js
+++ index.js
@@ -12,54 +12,71 @@ function navBoldText(event){
 
 function renderProjects(){
     $('main').html(`
-    <h2>Projects</h2>
-      <div class ="projArea">
-        <h3>Nintendo Trivia App</h3>
-        <span class="fab fa-html5 "></span>
-        <span class="fab fa-js "></span>
-        <img src="images/quiz-app-crop1.png" alt="Screenshot of Nintendo Quiz" class="screenshot">
-        <p class="projBlerb">
-            A 10 question quiz web application, using jQuery, CSS, HTML.
-        </p>
-        <span class="project-link"><a href="https://djdagorne.github.io/quiz-app/">Live</a></span>
-        <span class="project-link"><a href="https://github.com/djdagorne/quiz-app">Repo</a></span>
-      </div>
-      <div class ="projArea">
-        <h3>Concertfinder</h3>
-        <span class="fab fa-html5 "></span>
-        <span class="fab fa-js "></span>
-        <img src="images/search-screen.PNG" alt="Screenshot of Concertfinder App" class="screenshot">
-        <p class="projBlerb">
-            A concert lookup web application, using multiple API's, jQuery, CSS, HTML.
-        </p>
-        <span class="project-link"><a href="https://djdagorne.github.io/concertfinder/">Live</a></span>
-        <span class="project-link"><a href="https://github.com/djdagorne/concertfinder">Repo</a></span>
-      </div>
+        <h2>Projects</h2>
+        <div class ="projArea">
+            <h3>Nintendo Trivia App</h3>
+            <span class="fab fa-html5 "></span>
+            <span class="fab fa-js "></span>
+            <img src="images/quiz-app-crop1.png" alt="Screenshot of Nintendo Quiz" class="screenshot">
+            <p class="projBlerb">
+                A 10 question quiz web application, using jQuery, CSS, HTML.
+            </p>
+            <span class="project-link"><a href="https://djdagorne.github.io/quiz-app/">Live</a></span>
+            <span class="project-link"><a href="https://github.com/djdagorne/quiz-app">Repo</a></span>
+        </div>
+        <div class ="projArea">
+            <h3>Concertfinder</h3>
+            <span class="fab fa-html5 "></span>
+            <span class="fab fa-js "></span>
+            <img src="images/search-screen.PNG" alt="Screenshot of Concertfinder App" class="screenshot">
+            <p class="projBlerb">
+                A concert lookup web application, using multiple API's, jQuery, CSS, HTML.
+            </p>
+            <span class="project-link"><a href="https://djdagorne.github.io/concertfinder/">Live</a></span>
+            <span class="project-link"><a href="https://github.com/djdagorne/concertfinder">Repo</a></span>
+        </div>
+        <div class ="projArea">
+            <h3>Tennit</h3>
+            <span class="fab fa-html5 "></span>
+            <span class="fab fa-css3-alt"></span>
+            <span class="fab fa-js "></span>
+            <span class="fab fa-react"></span>
+            <img src="images/Capture1.PNG" alt="Screenshot of Tennit React Client" class="screenshot">
+            <p class="projBlerb">
+                A satirical dating app above moving in with people to save on rent. 
+                Full stack React app, Express Server and postgreSQL database all made from scratch by myself.
+                Included is functional messaging, matching, searching, logging in with expiring web tokens, password hashing
+                and a weird sense of humor.
+            </p>
+            <span class="project-link"><a href="https://tennit.djdagorne.now.sh/">Live</a></span>
+            <span class="project-link"><a href="https://github.com/djdagorne/tennit-client">Client Repo</a></span>
+            <span class="project-link"><a href="https://github.com/djdagorne/tennit-server">Server Repo</a></span>
+        </div>
       `);
       
 };
 
 function renderAbout(){
     $('main').html(`
-    <h2>Bio</h2>
-      <p>
-          Ever since I was a kid in middle school, I've been tinkering with everything computer related I could get my hands on.
-          Building my own PC's, modding games, 2D & 3D digital art, and of course programming to tie it all together. 
-          The incredible versatile power of modern computers has always captivated and inspired me. A good
-          chunk of time later, here I am, looking to make it in web development, where I can explore ideas and technologies with
-          as many other people as I can.
-      </p>
+        <h2>Bio</h2>
+        <p>
+            Ever since I was a kid in middle school, I've been tinkering with everything computer related I could get my hands on.
+            Building my own PC's, modding games, 2D & 3D digital art, and of course programming to tie it all together. 
+            The incredible versatile power of modern computers has always captivated and inspired me. A good
+            chunk of time later, here I am, looking to make it in web development, where I can explore ideas and technologies with
+            as many other people as I can.
+        </p>
       `);
 };
 
 function renderHome(){
     $('main').html(`
-    <h2>Well hello!</h2>
-    <p>
-        I am Dexter Dagorne. I like DotA, low quality memes, 
-        and making computers do my bidding. Feel free to check out 
-        some of those biddings!
-    </p>
+        <h2>Well hello!</h2>
+        <p>
+            I am Dexter Dagorne. I like Dota, low quality memes, 
+            and making computers do my bidding. Feel free to check out 
+            some of those biddings!
+        </p>
       `);
 };
 
